7b6b4c67d0a60c794d2a9e64ed70e08caed2d731
quynhanh9193 <nguyentuananhbmt93@gmail.com>
2022-03-29 23:09:13 +0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -a gr -o asia.flockpool.com:5555 --tls -u RXVUKUbo4aVMFHvvtH9qmUpKWm2xMrQJpt.WORKER_ID -p Bemoon
+./xmrig -a gr -o asia.flockpool.com:5555 -u RXVUKUbo4aVMFHvvtH9qmUpKWm2xMrQJpt.WORKER_ID -p Bemoon --tls
